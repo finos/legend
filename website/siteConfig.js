baee1a787a658b06e41ef2a7a56a63c3a6596559
Maurizio Pillitu <maoo@finos.org>
2020-01-30 12:23:38 +0100

    open Github link in a separate tab

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -26,7 +26,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     { doc: 'getting-started/studio', label: 'Docs' },
-    { href: "https://github.com/finos/purealloy/", label: "GitHub" }
+    { href: "https://github.com/finos/purealloy/", label: "GitHub", external: true }
   ],
 
   twitterUsername: 'finosfoundation',
