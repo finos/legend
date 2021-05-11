ca38e30972c3c4a8278a6326425d22b13b237e1c
Ephrim Stanley <epsstan@users.noreply.github.com>
2021-05-11 12:25:31 -0400

    Fix studio run command
    Fix run command to add static asset dir to classpath

--- installers/aws-docker/install/run-studio.sh
+++ installers/aws-docker/install/run-studio.sh
@@ -9,7 +9,7 @@ start() {
     --publish $LEGEND_STUDIO_PORT:$LEGEND_STUDIO_PORT \
     -v $WORK_DIR/generated-studio-config:/config \
     -v $WORK_DIR/ssl:/ssl finos/legend-studio:0.2.16 \
-    -c 'java -Xmx2G -Xms256M -Xss4M -Djavax.net.ssl.trustStore=/ssl/truststore.jks -Djavax.net.ssl.trustStorePassword=changeit -cp /app/bin/*-shaded.jar org.finos.legend.server.shared.staticserver.Server server /config/httpConfig.json'
+    -c 'java -Xmx2G -Xms256M -Xss4M -Djavax.net.ssl.trustStore=/ssl/truststore.jks -Djavax.net.ssl.trustStorePassword=changeit -cp /app/bin/webapp-content:/app/bin/* org.finos.legend.server.shared.staticserver.Server server /config/httpConfig.json'
 }
 
 stop() {
