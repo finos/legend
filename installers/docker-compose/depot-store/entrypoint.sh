dbe474202b85ce27acc4feaa77afafa3fc7da0da
Ephrim Stanley <ephrim.stanley@gs.com>
2023-02-12 00:13:28 -0500

    Update installer

--- installers/docker-compose/depot-store/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Container Start: $(time +%s) 2"
-
-ls /app/bin/*
-
-java -cp /config:/app/bin/* -XX:+ExitOnOutOfMemoryError -XX:MaxRAMPercentage=60 -Xss4M -Dfile.encoding=UTF8 org.finos.legend.depot.store.server.LegendDepotStoreServer server /config/config.json
--- installers/docker-compose/depot-store/setup/setup.sh
+++ installers/docker-compose/depot-store/setup/setup.sh
@@ -2,4 +2,4 @@
 
 sleep 5
 
-curl -v -X GET "http://legend-depot-store:${DEPOT_STORE_PORT}/depot-store/api/queue/Legend%20Query%20Demo/org.finos.legend.demo/legend-query/1.0.2?maxRetries=5" -H "accept: text/plain"
+curl -v -X GET "http://legend-depot-store:${DEPOT_STORE_PORT}/depot-store/api/queue/PROD-1234/org.finos.legend.demo/legend-query/1.0.2?maxRetries=5" -H "accept: text/plain"
