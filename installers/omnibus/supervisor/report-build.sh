ae9f4158f078eec43cfdf7a77972da35fd218d50
An Phi <aphi@skidmore.edu>
2023-05-24 02:26:27 -0400

    omnibus: cleanup to integrate with release workflow

--- installers/omnibus/supervisor/report-build.sh
+++ installers/omnibus/supervisor/report-build.sh
@@ -5,8 +5,8 @@ touch /.buildinfo.json
 echo "{" >> /.buildinfo.json
 echo "  \"version\": \"$LEGEND_OMNIBUS_IMAGE_VERSION\"," >> /.buildinfo.json
 echo "  \"gitlab_version\": \"$LEGEND_OMNIBUS_GITLAB_VERSION\"," >> /.buildinfo.json
-echo "  \"engine_image\": \"$LEGEND_OMNIBUS_ENGINE_IMAGE\"," >> /.buildinfo.json
-echo "  \"sdlc_image\": \"$LEGEND_OMNIBUS_SDLC_IMAGE\"," >> /.buildinfo.json
+echo "  \"engine_image\": \"$LEGEND_OMNIBUS_ENGINE_VERSION\"," >> /.buildinfo.json
+echo "  \"sdlc_image\": \"$LEGEND_OMNIBUS_SDLC_VERSION\"," >> /.buildinfo.json
 echo "  \"pure_ide_image\": \"$LEGEND_OMNIBUS_PURE_IDE_IMAGE\"," >> /.buildinfo.json
-echo "  \"studio_image\": \"$LEGEND_OMNIBUS_STUDIO_IMAGE\"" >> /.buildinfo.json
+echo "  \"studio_image\": \"$LEGEND_OMNIBUS_STUDIO_VERSION\"" >> /.buildinfo.json
 echo "}" >> /.buildinfo.json
