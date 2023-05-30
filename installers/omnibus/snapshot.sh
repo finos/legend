07843d4acd717c243e917aa770a39215ec3c9010
An Phi <aphi@skidmore.edu>
2023-05-30 15:45:35 -0400

    omnibus: run test and publish snapshots

--- installers/omnibus/snapshot.sh
+++ installers/omnibus/snapshot.sh
@@ -19,29 +19,36 @@ source ./.env
 # the setup is proper and the configs are up-to-date, if any components
 # make a breaking change in their config, this test will detect it
 
+echo "Running tests on the slim image..."
 bash ./build-slim.sh
-bash ./run-slim.sh &
-while :; do
-	# Check status of Supervisor component processes
-	status_supervisor_components=$(supervisorctl -c /app/supervisor/supervisor.conf status | awk '{print $2}')
-	if grep -q "$status_supervisor_components" <<< "EXITED"; then
-	  echo -e "\e[31mFAILED: Some component(s) have failed to start\e[0m"
-		exit 1
-		break
-	fi
+docker run \
+	--platform=linux/amd64 \
+	-dit \
+	-p $LEGEND_OMNIBUS_SUPERVISOR_PORT:$LEGEND_OMNIBUS_SUPERVISOR_PORT \
+	-p $LEGEND_OMNIBUS_SUPERVISOR_STATUS_CHECKER_PORT:$LEGEND_OMNIBUS_SUPERVISOR_STATUS_CHECKER_PORT \
+	-p $LEGEND_OMNIBUS_NGINX_PORT:$LEGEND_OMNIBUS_NGINX_PORT \
+	-p $LEGEND_OMNIBUS_GITLAB_PORT:$LEGEND_OMNIBUS_GITLAB_PORT \
+	-p $LEGEND_OMNIBUS_ENGINE_PORT:$LEGEND_OMNIBUS_ENGINE_PORT \
+	-p $LEGEND_OMNIBUS_SDLC_PORT:$LEGEND_OMNIBUS_SDLC_PORT \
+	-p $LEGEND_OMNIBUS_PURE_IDE_PORT:$LEGEND_OMNIBUS_PURE_IDE_PORT \
+	-p $LEGEND_OMNIBUS_STUDIO_PORT:$LEGEND_OMNIBUS_STUDIO_PORT \
+	--env LEGEND_OMNIBUS_REMOTE_GITLAB_PAT="$LEGEND_OMNIBUS_REMOTE_GITLAB_PAT" \
+	legend-omnibus:latest-slim
 
+while :; do
 	status_status_checker=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:$OMNIBUS_STATUS_REPORT_FILE_PATH/status)
 	if [[ $status_status_checker -ge 300 ]]; then
 	  echo -e "\e[31mFAILED: Status checker failed to start\e[0m"
 		exit 1
-		break
-	fi
-
-  status=$(curl --silent http://localhost:6903/status | jq '.status')
-	if [[ $status -eq "FAILED" ]]; then
-	  echo -e "\e[31mFAILED: Some status checks failed. Please identify and fix the issue... 🙏🙏🙏\e[0m"
-		exit 1
-		break
+	else
+		status=$(curl --silent http://localhost:$LEGEND_OMNIBUS_SUPERVISOR_STATUS_CHECKER_PORT/status | jq -r '.status')
+		if [[ $status == "FAILED" ]]; then
+			echo -e "\e[31mFAILED: Some status checks failed. Please identify and fix the issue... 🙏🙏🙏\e[0m"
+			exit 1
+		elif [[ $status == "SUCCEEDED" ]]; then
+			echo -e "\e[32mSUCCESS: All status checks passed! 🎉🎉🎉\e[0m"
+			break
+		fi
 	fi
 
 	sleep 10
@@ -56,32 +63,32 @@ done
 # so we will use `docker/login-action`, if we run this script manually, make sure we
 # login beforehand.
 
-# # ------------------------- Standard Variant -------------------------
+# ------------------------- Slim Variant -------------------------
+# This build skips Gitlab
 
-# bash ./build.sh
-# docker tag legend-omnibus:latest finos/legend-omnibus:snapshot
-# docker push --quiet finos/legend-omnibus:snapshot || {
-#   exit 1
-# }
-# docker tag legend-omnibus:latest finos/legend-omnibus:latest
-# docker push --quiet finos/legend-omnibus:latest || {
-#   exit 1
-# }
+docker tag legend-omnibus:latest-slim finos/legend-omnibus:snapshot-slim
+docker push --quiet finos/legend-omnibus:snapshot-slim || {
+  exit 1
+}
+docker tag legend-omnibus:latest-slim finos/legend-omnibus:latest-slim
+docker push --quiet finos/legend-omnibus:latest-slim || {
+  exit 1
+}
 
-# # ------------------------- Slim Variant -------------------------
-# # This build skips Gitlab
+# ------------------------- Standard Variant -------------------------
 
-# docker tag legend-omnibus:latest-slim finos/legend-omnibus:snapshot-slim
-# docker push --quiet finos/legend-omnibus:snapshot-slim || {
-#   exit 1
-# }
-# docker tag legend-omnibus:latest-slim finos/legend-omnibus:latest-slim
-# docker push --quiet finos/legend-omnibus:latest-slim || {
-#   exit 1
-# }
+bash ./build.sh
+docker tag legend-omnibus:latest finos/legend-omnibus:snapshot
+docker push --quiet finos/legend-omnibus:snapshot || {
+  exit 1
+}
+docker tag legend-omnibus:latest finos/legend-omnibus:latest
+docker push --quiet finos/legend-omnibus:latest || {
+  exit 1
+}
 
-# # ------------------------- Summary -------------------------
+# ------------------------- Summary -------------------------
 
-# echo -e "\n"
-# echo -e "${GREEN}Successfully published snapshot images to Docker Hub! ${NC}"
-# echo -e "\n"
+echo -e "\n"
+echo -e "${GREEN}Successfully published snapshot images to Docker Hub! ${NC}"
+echo -e "\n"
