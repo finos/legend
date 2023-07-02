cbe7fc5414ce5764007e8c267c86983181a9b62f
An Phi <aphi@skidmore.edu>
2023-07-02 14:34:01 -0400

    omnibus: update README

--- installers/omnibus/scripts/start.gitlab-pat.sh
+++ installers/omnibus/scripts/start.gitlab-pat.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 echo "Running Omnibus with Legend SDLC using Gitlab (PAT) backend..."
-if [[ -z "$LEGEND_OMNIBUS_CONFIG_GITLAB_PAT" ]]; then
-  echo "Failed: LEGEND_OMNIBUS_CONFIG_GITLAB_PAT is not set"
-  exit 1
+if [[ -z "$PS1" ]]; then
+  # non-interactive
+  if [[ -z "$LEGEND_OMNIBUS_CONFIG_GITLAB_PAT" ]]; then
+    echo "Failed: LEGEND_OMNIBUS_CONFIG_GITLAB_PAT is not set"
+    exit 1
+  fi
+else
+  # interactive
+  if [[ -z "$LEGEND_OMNIBUS_CONFIG_GITLAB_PAT" ]]; then
+    echo "Enter Gitlab personal access token (PAT):"
+    read -s LEGEND_OMNIBUS_CONFIG_GITLAB_PAT
+  fi
 fi
 
-if [[ "$EXPOSE_BACKEND_PORTS" = true ]]; then
+if [[ "$LEGEND_OMNIBUS_CONFIG_EXPOSE_BACKEND_PORTS" = true ]]; then
   docker run --platform=linux/amd64 -it -p 6900:6900 -p 6100:6100 -p 6300:6300 \
     --env LEGEND_OMNIBUS_CONFIG_SDLC_MODE="gitlab-pat" \
     --env LEGEND_OMNIBUS_CONFIG_GITLAB_PAT="$LEGEND_OMNIBUS_CONFIG_GITLAB_PAT" \
--- installers/omnibus/scripts/start.sh
+++ installers/omnibus/scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$EXPOSE_BACKEND_PORTS" = true ]]; then
+if [[ "$LEGEND_OMNIBUS_CONFIG_EXPOSE_BACKEND_PORTS" = true ]]; then
   docker run --platform=linux/amd64 -it -p 6900:6900 -p 6100:6100 -p 6300:6300 \
   finos/legend-omnibus:latest-slim
 else
