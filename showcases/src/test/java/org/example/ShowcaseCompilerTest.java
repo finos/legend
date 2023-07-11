4f15e87392ec52b39d50d11f9745367dc3946570
Barbosa Teixeira Santos, Pedro [Engineering] <Pedro.BarbosaTeixeira@ny.email.gs.com>
2023-07-11 11:21:29 -0400

    Run tests for showcases
    Adds workflow to test changes to showcases twice daily, on PRs or commit on master.
    
    Showcase tests consist of a roundtrip compilation test with latest legend versions to enforce compilation and formatting. Does not yet allow for comments.
    
    The paths in the poms and the test file are different due to the
    location that workflows vs tests checkout the code.
    
    Showcase code moved to not clog up directory structure.

---
+++ showcases/src/test/java/org/example/ShowcaseCompilerTest.java
@@ -0,0 +1,89 @@
+package org.example;
+
+import org.eclipse.collections.api.factory.Sets;
+import org.finos.legend.engine.language.pure.grammar.from.PureGrammarParser;
+import org.finos.legend.engine.language.pure.grammar.to.PureGrammarComposer;
+import org.finos.legend.engine.language.pure.grammar.to.PureGrammarComposerContext;
+import org.finos.legend.engine.protocol.pure.v1.model.context.PureModelContextData;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit tests for each showcase that will run in parallel reading from file
+ */
+@RunWith(Parameterized.class)
+public class ShowcaseCompilerTest
+{
+    private static final PathMatcher PURE_FILE_MATCHER = FileSystems.getDefault().getPathMatcher("glob:*.pure");
+    @Parameter
+    public Path showcasePath;
+
+    @Parameters(name = "{0}")
+    public static Iterable<?> showcaseFilePathsToTest()
+    {
+        final String[] SHOWCASE_LOCATIONS = System.getProperty("showcase.locations", "data").split(",");
+
+        Set<Path> showcasePaths = Sets.mutable.empty();
+
+        for (String location : SHOWCASE_LOCATIONS)
+        {
+            Path path = Paths.get(location);
+            if (Files.isDirectory(path))
+            {
+                showcasePaths.addAll(getShowcaseFilePaths(path));
+            }
+            else if (PURE_FILE_MATCHER.matches(path.getFileName()))
+            {
+                showcasePaths.add(path);
+            }
+        }
+
+        return showcasePaths;
+    }
+
+    private static Set<Path> getShowcaseFilePaths(Path showcaseDirectory)
+    {
+        try (Stream<Path> pathStream = Files.walk(showcaseDirectory))
+        {
+            return pathStream
+                    .filter(p -> !Files.isDirectory(p))
+                    .filter(p -> PURE_FILE_MATCHER.matches(p.getFileName()))
+                    .collect(Collectors.toSet());
+        }
+        catch (IOException e)
+        {
+            throw new IllegalStateException(String.format("Invalid directory name %s.", showcaseDirectory), e);
+        }
+    }
+
+    /**
+     * Tests that a file can be processed by the new Legend version
+     */
+    @Test
+    public void processShowcaseFile() throws IOException
+    {
+        String pureGrammar = String.join("\n", Files.readAllLines(showcasePath));
+
+        // TODO: Ideally want compilation and all testables to be run while maintaining comments in the code
+        PureModelContextData pureModelContextData = PureGrammarParser.newInstance().parseModel(pureGrammar, "", 0, 0, true);
+        PureGrammarComposer grammarComposer = PureGrammarComposer.newInstance(PureGrammarComposerContext.Builder.newInstance().build());
+
+        // Grammar composer adds a trailing newline
+        assertEquals(pureGrammar + "\n", grammarComposer.renderPureModelContextData(pureModelContextData));
+    }
+}
