36d803055d691e5842eba675e32f919f4a1c2c81
An Phi <aphi@skidmore.edu>
2023-07-28 12:19:13 -0400

    update workflow for showcases (#726)
    * update workflow for showcases
    
    * minor fixes
    
    * minor reorg
    
    * minor fixeS

--- scripts/updateShowcasesData.js
+++ scripts/updateShowcasesData.js
@@ -1,7 +1,8 @@
-import { extractShowcaseRegistryData } from "@finos/legend-server-showcase-deployment/builder";
+import { buildShowcaseRegistryData } from "@finos/legend-server-showcase-deployment/builder";
 import { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
-import { mkdirSync, existsSync } from "fs";
+import { mkdirSync, existsSync, writeFileSync } from "fs";
+import { execSync } from "child_process";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const outputDir = resolve(__dirname, "../website/static/showcases");
@@ -13,9 +14,11 @@ if (!existsSync(outputDir)) {
 const startTime = Date.now();
 console.log(`Building showcase registry data...`);
 
-extractShowcaseRegistryData(
-  resolve(__dirname, "../showcases/data"),
-  resolve(outputDir, "data.json")
+const data = buildShowcaseRegistryData(resolve(__dirname, "../showcases/data"));
+writeFileSync(resolve(outputDir, "data.json"), JSON.stringify(data));
+
+const hasChanged = Boolean(
+  execSync("git status --porcelain", { encoding: "utf-8" }).trim()
 );
 
 console.log(
