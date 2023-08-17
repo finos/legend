8209f46189964e725a6e5d756f690d2aec819295
Hardik Maheshwari <19693874+hardikmaheshwari@users.noreply.github.com>
2023-08-17 17:05:05 +0530

    Uplift stack release workflow to auto-compute versions (#738)
    * Uplift stack release workflow to auto-compute versions
    
    * fix inputs
    
    * Use tag api
    
    * Use bash script to compute version
    
    * Hardcode owner in script
    
    * Fix version calculation for major and minor release

---
+++ scripts/compute-version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+REPO_NAME="$1"
+RELEASE_TYPE="$2"
+
+# Query Maven Central API to get latest version
+latest_version=$(curl -s "https://api.github.com/repos/finos/${REPO_NAME}/tags" | jq -r --arg repo_name "${REPO_NAME}" '.[] | select(.name | test("^" + $repo_name + "-[0-9]+\\.[0-9]+\\.[0-9]+$")) | .name' | sort -V | tail -n1 | sed "s/^${REPO_NAME}-//")
+
+# Increment patch version
+IFS='.' read -r major minor patch <<< "$latest_version"
+
+if [ "$RELEASE_TYPE" == "major" ]; then
+  major=$((major + 1))
+  minor=0
+  patch=0
+elif [ "$RELEASE_TYPE" == "minor" ]; then
+  minor=$((minor + 1))
+  patch=0
+else
+  patch=$((patch + 1))
+fi
+
+new_version="${major}.${minor}.${patch}"
+echo "::set-output name=new_version::$new_version"
