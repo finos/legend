42726c281f6f740f5ea9258e5509853cb3af13a9
An Phi <aphi@skidmore.edu>
2023-09-07 15:52:40 -0400

    omnibus: fix postgres connection for ghc variant (#748)

--- installers/omnibus/components/datasources/run-postgres.ghc.sh
+++ installers/omnibus/components/datasources/run-postgres.ghc.sh
@@ -3,7 +3,9 @@
 # Preconfigure Postgres to make sure it allows incoming connections
 # See https://stackoverflow.com/a/3278835
 sed -i "s/^#listen_addresses = .*/listen_addresses = '*'/g" /etc/postgresql/14/main/postgresql.conf
-echo 'host    all             all             0.0.0.0/0           scram-sha-256' >> /etc/postgresql/14/main/pg_hba.conf
+echo 'host    all             all             0.0.0.0/0           trust' >> /etc/postgresql/14/main/pg_hba.conf
+sed -i "s/scram-sha-256/trust/g" /etc/postgresql/14/main/pg_hba.conf
+sed -i "s/peer/trust/g" /etc/postgresql/14/main/pg_hba.conf
 
 echo -e "\nSetting up PostgreSQL datasource..."
 
