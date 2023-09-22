0e62d58efac810ad89c91b1b1fe980f4745395b2
Ephrim Stanley <epsstan@users.noreply.github.com>
2023-09-22 16:44:31 -0400

    Update start.ghc.sh

--- installers/omnibus/scripts/start.ghc.sh
+++ installers/omnibus/scripts/start.ghc.sh
@@ -8,7 +8,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No color
 # --------------------------------------------------------------------
 
-IMAGE=finos/legend-omnibus:latest-ghc
+IMAGE=finos/legend-omnibus:2023-09-15-ghc
 
 # ------------------------------- Check -------------------------------
 # Check if repulling image is necessary
